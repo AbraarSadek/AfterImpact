624b4efdd685c00d10ee87620abbf88b3721d99e
VinceZer0 <127914102+VinceZer0@users.noreply.github.com>
2025-09-30 15:53:35 -0400

    Basic Player Controllers - 09/30/2025
    1. Added basic movement to player character

---
+++ After Impact/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+    public float playerSpeed;
+    private Rigidbody2D rb;  // ❗ Corrected casing (was RigidBody2D)
+    private Vector2 playerDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // ❗ Corrected casing
+    }
+
+    void Update()
+    {
+        float directionY = Input.GetAxisRaw("Vertical");
+        playerDirection = new Vector2(0, directionY).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(0, playerDirection.y * playerSpeed); // ❗ Removed incorrect dot
+    }
+}
