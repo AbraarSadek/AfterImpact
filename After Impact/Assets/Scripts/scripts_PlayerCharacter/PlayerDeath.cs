269df89a2379883d807a1256cb7e48c92f2d25a4
Drew <72636893+CallMeDrewChristian@users.noreply.github.com>
2025-10-30 14:08:49 -0400

    Added PlayerDeath Scene & PlayerDeath Script
    Co-Authored-By: Abraar Sadek <132792374+AbraarSadek@users.noreply.github.com>

---
+++ After Impact/Assets/Scripts/scripts_PlayerCharacter/PlayerDeath.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+/*
+ * Created Date: 10/30/2025
+ * Created By: Drew Oro
+ * Purpose: This script is responsible for handling PlayerDeath from the collision with the meteorites.
+ */
+
+public class PlayerDeath : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
